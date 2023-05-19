d61a2563fabaa6b4f79bf741cbee3b3b15a9d105
Oussama Rehahla <129313056+ouxava@users.noreply.github.com>
2023-05-19 16:53:06 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,153 +1,127 @@
-<?php require_once('./config.php'); ?>
- <!DOCTYPE html>
-<html lang="en" class="" style="height: auto;">
-<style>
-  #header{
-    height:70vh;
-    width:calc(100%);
-    position:relative;
-    top:-1em;
-  }
-  #header:before{
-    content:"";
-    position:absolute;
-    height:calc(100%);
-    width:calc(100%);
-    background-image:url(<?= validate_image($_settings->info("cover")) ?>);
-    background-size:cover;
-    background-repeat:no-repeat;
-    background-position: center center;
-  }
-  #header>div{
-    position:absolute;
-    height:calc(100%);
-    width:calc(100%);
-    z-index:2;
-  }
+<?php 
+$category_ids = isset($_GET['cids']) ? $_GET['cids'] : 'all';
+?>
+<div class="content py-3">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-outline rounded-0 card-primary shadow">
+                <div class="card-body">
+                    <div class="list-group">
+                        <div class="list-group-item list-group-item-action">
+                            <div class="custom-control custom-checkbox">
+                                <input class="custom-control-input custom-control-input-primary custom-control-input-outline cat_all" type="checkbox" id="cat_all" <?= !is_array($category_ids) && $category_ids =='all' ? "checked" : "" ?>>
+                                <label for="cat_all" class="custom-control-label"> All</label>
+                            </div>
+                        </div>
+                        <?php 
+                        $categories = $conn->query("SELECT * FROM `category_list` where delete_flag = 0 and status = 1 order by `name` asc ");
+                        while($row = $categories->fetch_assoc()):
+                        ?>
+                        <div class="list-group-item list-group-item-action">
+                            <div class="custom-control custom-checkbox">
+                                <input class="custom-control-input custom-control-input-primary custom-control-input-outline cat_item" type="checkbox" id="cat_item<?= $row['id'] ?>" <?= in_array($row['id'],explode(',',$category_ids)) ? "checked" : '' ?> value="<?= $row['id'] ?>">
+                                <label for="cat_item<?= $row['id'] ?>" class="custom-control-label"> <?= $row['name'] ?></label>
+                            </div>
+                        </div>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="col-md-8">
+            <div class="card card-outline card-primary shadow rounded-0">
+                <div class="card-body">
+                    <div class="container-fluid">
+                        <div class="row justify-content-center mb-3">
+                            <div class="col-lg-8 col-md-10 col-sm-12">
+                                <form action="" id="search-frm">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend"><span class="input-group-text">Search</span></div>
+                                        <input type="search" id="search" class="form-control" value="<?= isset($_GET['search']) ? $_GET['search'] : '' ?>">
+                                        <div class="input-group-append"><span class="input-group-text"><i class="fa fa-search"></i></span></div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="row" id="product_list">
+                            <?php 
+                            $swhere = "";
+                            if(!empty($category_ids)):
+                            if($category_ids !='all'){
+                                $swhere = " and p.category_id in ({$category_ids}) ";
+                            }
+                            if(isset($_GET['search']) && !empty($_GET['search'])){
+                                $swhere .= " and (p.name LIKE '%{$_GET['search']}%' or p.description LIKE '%{$_GET['search']}%' or c.name LIKE '%{$_GET['search']}%' or v.shop_name LIKE '%{$_GET['search']}%') ";
+                            }
 
-  #top-Nav a.nav-link.active {
-      color: #343a40;
-      font-weight: 900;
-      position: relative;
-  }
-  #top-Nav a.nav-link.active:before {
-    content: "";
-    position: absolute;
-    border-bottom: 2px solid #343a40;
-    width: 33.33%;
-    left: 33.33%;
-    bottom: 0;
-  }
-  @media (max-width:760px){
-    #top-Nav a.nav-link.active {
-      background: #343a40db;
-      color: #fff;
-    }
-    #top-Nav a.nav-link.active:before {
-      content: "";
-      position: absolute;
-      border-bottom: 2px solid #343a40;
-      width: 100%;
-      left: 0;
-      bottom: 0;
-    }
-    h1.w-100.text-center.site-title.px-5{
-      font-size:2.5em !important;
-    }
-  }
-</style>
-<?php require_once('inc/header.php') ?>
-  <body class="layout-top-nav layout-fixed layout-navbar-fixed" style="height: auto;">
-    <div class="wrapper">
-     <?php $page = isset($_GET['page']) ? $_GET['page'] : 'home';  ?>
-     <?php require_once('inc/topBarNav.php') ?>
-     <?php if($_settings->chk_flashdata('success')): ?>
-      <script>
-        alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-      </script>
-      <?php endif;?>    
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper pt-5" style="">
-        <?php if($page == "home" || $page == "about"): ?>
-          <div id="header" class="shadow mb-4">
-              <div class="d-flex justify-content-center h-100 w-100 align-items-center flex-column px-3">
-                  <h1 class="w-100 text-center site-title px-5"><?php echo $_settings->info('name') ?></h1>
-                  <!-- <h3 class="w-100 text-center px-5 site-subtitle"><?php echo $_settings->info('name') ?></h3> -->
-              </div>
-          </div>
-        <?php endif; ?>
-        <!-- Main content -->
-        <section class="content ">
-          <div class="container">
-            <?php 
-              if(!file_exists($page.".php") && !is_dir($page)){
-                  include '404.html';
-              }else{
-                if(is_dir($page))
-                  include $page.'/index.php';
-                else
-                  include $page.'.php';
+                            $products = $conn->query("SELECT p.*, v.shop_name as vendor, c.name as `category` FROM `product_list` p inner join vendor_list v on p.vendor_id = v.id inner join category_list c on p.category_id = c.id where p.delete_flag = 0 and p.`status` =1 {$swhere} order by RAND()");
+                            while($row = $products->fetch_assoc()):
+                            ?>
+                            <div class="col-lg-4 col-md-6 col-sm-12 product-item">
+                                <a href="./?page=products/view_product&id=<?= $row['id'] ?>" class="card shadow rounded-0 text-reset text-decoration-none">
+                                <div class="product-img-holder position-relative">
+                                    <img src="<?= validate_image($row['image_path']) ?>" alt="Product-image" class="img-top product-img bg-gradient-gray">
+                                </div>
+                                    <div class="card-body border-top border-gray">
+                                        <h5 class="card-title text-truncate w-100"><?= $row['name'] ?></h5>
+                                        <div class="d-flex w-100">
+                                            <div class="col-auto px-0"><small class="text-muted">Entreprise: </small></div>
+                                            <div class="col-auto px-0 flex-shrink-1 flex-grow-1"><p class="text-truncate m-0"><small class="text-muted"><?= $row['vendor'] ?></small></p></div>
+                                        </div>
+                                        <div class="d-flex">
+                                            <div class="col-auto px-0"><small class="text-muted">Category: </small></div>
+                                            <div class="col-auto px-0 flex-shrink-1 flex-grow-1"><p class="text-truncate m-0"><small class="text-muted"><?= $row['category'] ?></small></p></div>
+                                        </div>
+                                        
+                                        <p class="card-text truncate-3 w-100"><?= strip_tags(html_entity_decode($row['description'])) ?></p>
+                                    </div>
+                                </a>
+                            </div>
+                            <?php endwhile; ?>
+                            <?php else: ?>
+                                <div class="col-12 text-center">
+                                    Pleas select atleast 1 stage category
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(function(){
+        if($('#cat_all').is(':checked') == true){
+            $('.cat_item').prop('checked',true)
+        }
+        if($('.cat_item:checked').length == $('.cat_item').length){
+            $('#cat_all').prop('checked',true)
+        }
+        $('.cat_item').change(function(){
+            var ids = [];
+            $('.cat_item:checked').each(function(){
+                ids.push($(this).val())
+            })
+            location.href="./?page=products&cids="+(ids.join(","))
+        })
+        $('#cat_all').change(function(){
+            if($(this).is(':checked') == true){
+                $('.cat_item').prop('checked',true)
+            }else{
+                $('.cat_item').prop('checked',false)
+            }
+            $('.cat_item').trigger('change')
+        })
+        $('#search-frm').submit(function(e){
+            e.preventDefault()
+            var q = "search="+$('#search').val()
+            if('<?= !empty($category_ids) && $category_ids !='all' ?>' == 1){
+                q += "&cids=<?= $category_ids ?>"
+            }
+            location.href="./?page=products&"+q;
 
-              }
-            ?>
-          </div>
-        </section>
-        <!-- /.content -->
-  <div class="modal fade rounded-0" id="uni_modal" role='dialog'>
-    <div class="modal-dialog modal-md modal-dialog-centered rounded-0" role="document">
-      <div class="modal-content rounded-0">
-        <div class="modal-header rounded-0">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body rounded-0">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade rounded-0" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header rounded-0">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body rounded-0">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade rounded-0" id="uni_modal_right" role='dialog'>
-    <div class="modal-dialog modal-full-height  modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header rounded-0">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-right"></span>
-        </button>
-      </div>
-      <div class="modal-body rounded-0">
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="viewer_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
-              <img src="" alt="">
-      </div>
-    </div>
-  </div>
-      </div>
-      <!-- /.content-wrapper -->
-      <?php require_once('inc/footer.php') ?>
-  </body>
-</html>
+        })
+    })
+</script>
